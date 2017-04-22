ed16e89232d809b3b8f7fc3490489d163b46b2f0
Chung-Ju Wu <jason.wu@easystack.cn>
2017-04-23 02:46:50 +0800

    Create a build_iso.sh that builds iso image.
    Using --chroot can help to log command process.
    Also it makes easier to change working directory with --cwd option.

---
+++ build_iso.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run the common jobs first.
+source ./common.sh
+
+# In order to create iso image, We create buildiso directory relative to chroot.
+/usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --chroot "rm -rf /buildiso; mkdir /buildiso"
+
+# Issue the lorax command to create iso image.
+# Instead of using --shell, we use --chroot because it can be logged
+# under /var/lib/mock/ and the --cwd is able to take effect.
+/usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --cwd="buildiso" \
+--chroot "lorax -p CentOS -v 7.3 -r 7.3 -s http://localhost/ESCore/CentOS/7.3.1611/os/x86_64 --isfinal /buildiso/result"
+
+# Finally we copy the result iso out of mock environment.
+/usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --copyout /buildiso/result/images/boot.iso escore.iso

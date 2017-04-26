017c8dd28a249bf70a48de9503ae5bd851d5709e
Chung-Ju Wu <jason.wu@easystack.cn>
2017-04-26 21:59:47 +0800

    Have our product name "NewESCore".

--- build_iso.sh
+++ build_iso.sh
@@ -13,7 +13,7 @@ source ./common.sh
 # Instead of using --shell, we use --chroot because it can be logged
 # under /var/lib/mock/ and the --cwd is able to take effect.
 /usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --cwd="buildiso" \
---chroot "lorax -p CentOS -v 7.3 -r 7.3 -s http://localhost/ESCore/CentOS/7.3.1611/os/x86_64 --isfinal /buildiso/result"
+--chroot "lorax -p NewESCore -v 7.3 -r 7.3 -s http://localhost/ESCore/CentOS/7.3.1611/os/x86_64 --isfinal /buildiso/result"
 
 # Finally we copy the result iso out of mock environment.
 /usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --copyout /buildiso/result/images/boot.iso escore.iso

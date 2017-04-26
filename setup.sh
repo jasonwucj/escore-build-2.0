d72b04fa7808958ca3113cc85975a3f32b230846
Chung-Ju Wu <jason.wu@easystack.cn>
2017-04-26 21:59:47 +0800

    Install EasyStack lorax package in mock environment.

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ source ./common.sh
 
 # Initialize mock environment.
 /usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --init
+
+# We need install our EasyStack lorax which is modified
+# to build ESCore installation iso.
+/usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --install http://localhost/ESCore/CentOS/7.3.1611/easystack/x86_64/Packages/lorax-19.6.78-1.el7.1.x86_64.rpm

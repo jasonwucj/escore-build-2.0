76f4586d8fe460ee4041ba450d0da811111f22d1
Chung-Ju Wu <jason.wu@easystack.cn>
2017-05-11 01:48:51 +0800

    Make the directory name of escore_repo consistent in both host and mock environment.

--- build_iso.sh
+++ build_iso.sh
@@ -10,7 +10,7 @@ rsync -avrtHu --delete rsync://${SERVER}/escore_repo ./escore_repo
 /usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --chroot "rm -rf /buildiso; mkdir /buildiso"
 
 # Copy the packages into mock environment and those rpm files will be included in the iso image.
-/usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --copyin escore_repo/ /buildiso/repo
+/usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --copyin escore_repo/ /buildiso/escore_repo
 
 # We need install our EasyStack lorax which is modified to build ESCore installation iso.
 /usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --install ${SERVER_EASYSTACK_REPO}/Packages/lorax-19.6.78-1.el7.1.x86_64.rpm
@@ -21,7 +21,7 @@ rsync -avrtHu --delete rsync://${SERVER}/escore_repo ./escore_repo
 # Also, our lorax support --customfield option and we can use it to specify the escore rpm files
 # which will be included in the iso image so that we have local media repository at installation stage.
 /usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --cwd="buildiso" \
---chroot "lorax -p ESCore -v 7.3 -r 7.3 -s ${SERVER_OS_REPO} -s ${SERVER_EASYSTACK_REPO} --isfinal --customfield /buildiso/repo /buildiso/result"
+--chroot "lorax -p ESCore -v 7.3 -r 7.3 -s ${SERVER_OS_REPO} -s ${SERVER_EASYSTACK_REPO} --isfinal --customfield /buildiso/escore_repo /buildiso/result"
 
 # Finally we copy the result iso out of mock environment.
 /usr/bin/mock -r centos-7-x86_64.cfg --rootdir `pwd`/chroot/ --copyout /buildiso/result/images/boot.iso escore.iso

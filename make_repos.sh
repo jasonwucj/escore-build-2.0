7f3b97740a1885c91da9d9bfbb5a1d059dd2e4c2
Chung-Ju Wu <jason.wu@easystack.cn>
2017-05-12 11:21:54 +0800

    We would like to build rpm files under mock environment. So call rpmgen_mock.sh instead.
    Certainly, the packages should provide rpmgen_mock.sh for others
    to easily create source rpm and binary rpm files.

--- make_repos.sh
+++ make_repos.sh
@@ -3,6 +3,10 @@
 # Run the common jobs first.
 source ./common.sh
 
+# Set variables that are going to be passed to rpmgen_mock.sh script.
+MOCK_CFG=`pwd`/centos-7-x86_64.cfg
+MOCK_CHROOT=`pwd`/chroot/
+
 # Set directory variables.
 ES_PKGSRC_DIR=`pwd`/easystack/pkg_src/
 ES_X8664_DIR=`pwd`/easystack/x86_64/
@@ -19,9 +23,10 @@ mkdir -p ${ES_X8664_PACKAGES_DIR}
 cd ${ES_PKGSRC_DIR}
 for REPO in ${REPO_ARRAY[@]}; do
   git clone ${REPO_LOCATION}/${REPO}.git
-  # For each package, we call rpmgen.sh to create its rpm and source rpm.
+  # For each package, we call rpmgen_mock.sh to create its source rpm
+  # and binary rpm under mock environment.
   cd ./${REPO}/
-  ./rpmgen.sh
+  ./rpmgen_mock.sh ${MOCK_CFG} ${MOCK_CHROOT}
   # Find all the binary rpm files and copy them into ES_X8664_PACKAGES_DIR directory.
   # Note that we employ "-exec" with ending "\;" to perform file copy operation.
   # See the manpage of find command for the details.

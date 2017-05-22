f0e45a8d7c9a096dfc1cc8a2876536a742dbf02d
Chung-Ju Wu <jason.wu@easystack.cn>
2017-05-23 01:51:18 +0800

    The setup.sh now accept two options: --config= and --server=
    --config=XXX
      It selects the configuration files under ./config/ directory.
      By default it select "default" configuration.
    
    --server=YYY
      If user applies this option, the SERVER field in common.sh will be substituted.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,49 @@
 #!/bin/bash
 
-# User must specify one configuration.
-# The available configurations can be found under ./config/ directory.
-# Then we will copy the settings to current location so that
-# other shell scripts are able to use them.
-if [ ${#} -ne 1 ]; then
-  echo ""
-  echo "Usage: ${0} CONFIG"
-  echo ""
-  echo "  CONFIG	check ./config/ for available configurations"
-  echo ""
-  exit 1
-fi
+# By default our configuration is "default".
+SET_CONFIG=default
 
-cp `pwd`/config/${1}/centos-7-x86_64.cfg .
-cp `pwd`/config/${1}/common.sh .
+# By default our server address is empty.
+# If this is still empty after parsing arguments,
+# we keep the original value unchanged in common.sh.
+SET_SERVER=
+
+
+# Parse the arguments.
+# Curretnly we only adopt --config= and --server= options.
+# The ${ARG#*=} means we would like to delete shortest match
+# of "*=" substring from front of the original ${ARG} string.
+# e.g. Assume ${ARG} is "--server=10.60.0.129", then ${ARG#*=} is "10.60.0.129".
+# See "Substring Removal" section in http://tldp.org/LDP/abs/html/string-manipulation.html
+for ARG in "$@"
+do
+  case ${ARG} in
+    --config=*)
+      SET_CONFIG="${ARG#*=}"
+      ;;
+    --server=*)
+      SET_SERVER="${ARG#*=}"
+      ;;
+    *)
+      # Unknown option.
+      echo "Unknown: ${ARG}"
+      ;;
+  esac
+done
+
+
+# Copy the configuration files.
+cp `pwd`/config/${SET_CONFIG}/centos-7-x86_64.cfg .
+cp `pwd`/config/${SET_CONFIG}/common.sh .
 echo "Copying settings ...... DONE."
+
+# Substitute SERVER field if user specifies it.
+# Also we need to use double quotes so that
+# the variable within string can be evaluated.
+if [ -n "${SET_SERVER}" ]; then
+  sed -i "s/^SERVER=.*/SERVER=${SET_SERVER}/g" common.sh
+  echo "Setting SERVER ...... DONE."
+fi
 
 # Run the common jobs first.
 source ./common.sh
